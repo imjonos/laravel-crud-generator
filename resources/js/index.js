90372f21156f46f0c4f8f36997553508f9457cb2
Eugeny Nosenko <info@coders.studio>
2020-01-15 23:28:13 +0300

    Update index.js
    VeeValidate fixed

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,12 +1,12 @@
 import BootstrapVue from 'bootstrap-vue'
 import lodash from 'lodash';
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import Notifications from 'vue-notification'
-import VeeValidate from 'vee-validate'
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import Notifications from 'vue-notification';
+import * as  VeeValidate from 'vee-validate';
 import flatPickr from 'vue-flatpickr-component';
 import 'flatpickr/dist/flatpickr.css';
-import vue2Dropzone from 'vue2-dropzone'
-import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+import vue2Dropzone from 'vue2-dropzone';
+import 'vue2-dropzone/dist/vue2Dropzone.min.css';
 import { VueEditor } from "vue2-editor/dist/vue2-editor.common";
 
 Object.defineProperty(Vue.prototype, '_', { value: lodash });
@@ -15,8 +15,8 @@ Vue.component('import', require('./components/Import.vue').default);
 Vue.component('vue-dropzone', vue2Dropzone);
 Object.defineProperty(Vue.prototype, '_', { value: lodash });
 require('./index');
-Vue.use(Notifications)
-Vue.use(BootstrapVue)
+Vue.use(Notifications);
+Vue.use(BootstrapVue);
 Vue.use(VeeValidate);
 Vue.use(flatPickr);
 VueEditor.methods.emitImageInfo = function emitImageInfo($event) {
@@ -31,7 +31,7 @@ VueEditor.methods.emitImageInfo = function emitImageInfo($event) {
     var cursorLocation = range.index;
     this.$emit("imageadded", file, Editor, cursorLocation, resetUploader);
 }
-Vue.component('vue-editor', VueEditor)
+Vue.component('vue-editor', VueEditor);
 
 const mixin = {
     methods: {
