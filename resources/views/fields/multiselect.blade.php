1a70719190c1400e8049bc3bf6ecb227ed9ca2cd
Eugeny Nosenko <Imnosok@gmail.com>
2021-04-16 14:26:51 +0300

    Update multiselect.blade.php

--- resources/views/fields/multiselect.blade.php
+++ resources/views/fields/multiselect.blade.php
@@ -1,21 +1,22 @@
 <div class="form-group">
-	<label for="">
-		{{ $label }}
-		@if($required ?? false)
-		<span class="text-danger">*</span>
-		@endif
-	</label>
-	<multi-select v-model="{{ $vModel }}"
-                  name="{{ $name }}"
-                  @if(isset($options))
-                    :options="{{ $options }}"
-                  @endif
-                  @if(isset($resourceUrl))
-                    :resource-url="{{ $resourceUrl }}"
-                  @endif
-                  :multiple="{{ (isset($multiple))?$multiple:'true' }}"
-
-                  placeholder="{{ $placeholder }}">
+    <label for="">
+        {{ $label }}
+        @if($required ?? false)
+            <span class="text-danger">*</span>
+        @endif
+    </label>
+    <multi-select
+        v-model="{{ $vModel }}"
+        name="{{ $name }}"
+        @if(isset($options))
+        :options="{{ $options }}"
+        @endif
+        @if(isset($resourceUrl))
+        :resource-url="{{ $resourceUrl }}"
+        @endif
+        :multiple="{{ (isset($multiple))?$multiple:'true' }}"
+        :allow-empty="{{ ($required)?'false':'true' }}"
+        placeholder="{{ $placeholder }}">
     </multi-select>
-	<div class="text-danger" v-if="errors.has('{{ $name }}')" v-html="errors.first('{{ $name }}')"></div>
+    <div class="text-danger" v-if="errors.has('{{ $name }}')" v-html="errors.first('{{ $name }}')"></div>
 </div>
