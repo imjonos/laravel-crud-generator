2d1de49c0fe505e48d3e89f11011f8a9c27334a0
Eugeny Nosenko <info@coders.studio>
2022-06-15 14:20:46 +0300

    Added new view stubs

--- resources/js/components/MultiSelect.vue
+++ resources/js/components/MultiSelect.vue
@@ -1,37 +1,37 @@
 <template>
     <div>
         <vue-multiselect v-model="selected"
-                         :placeholder="placeholder"
-                         :preserve-search="true"
-                         open-direction="bottom"
-                         label="name"
-                         track-by="name"
-                         :options="data"
+                         :allowEmpty="allowEmpty"
+                         :multiple="multiple"
                          :name="name"
+                         :options="data"
+                         :placeholder="placeholder"
+                         :preselect-first="false"
+                         :preserve-search="true"
                          :show-labels="false"
                          :taggable="multiple"
                          deselectLabel="x"
+                         label="name"
+                         open-direction="bottom"
                          selectLabel=""
                          selectedLabel=""
                          tagPlaceholder=""
-                         :preselect-first="false"
-                         :allowEmpty="allowEmpty"
-                         @search-change="getData"
-                         :multiple="multiple">
+                         track-by="id"
+                         @search-change="getData">
             <template slot="singleLabel" slot-scope="props">
-                {{ props.option.name }}
+                {{ getItemTitle(props.option) }}
             </template>
             <template slot="tag" slot-scope="props">
                 <span class="multiselect__tag">
-                    <span> {{ props.option.name }} </span>
-                    <i aria-hidden="true" @click="removeTag(props.option.id)" tabindex="1"
-                       class="multiselect__tag-icon"></i>
+                    <span> {{ getItemTitle(props.option) }} </span>
+                    <i aria-hidden="true" class="multiselect__tag-icon" tabindex="1"
+                       @click="removeTag(props.option.id)"></i>
                 </span>
             </template>
             <template slot="option" slot-scope="props">
                 <div class="option__desc">
                    <span class="option__title">
-                        {{ props.option.name }}
+                        {{ getItemTitle(props.option) }}
                    </span>
                 </div>
             </template>
@@ -83,15 +83,33 @@ export default {
                 {id: '1', name: 'Option'},
             ]
         },
+        labelAttribute: {
+            type: String,
+            default: "asciiname"
+        },
         resourceUrl: {
             type: String,
             default: ""
+        },
+        useQuery: {
+            type: Boolean,
+            default: false
         }
     },
     mounted() {
-        this.getData();
-        if(!this.multiple && this.value) {
-            this.getItem(this.value);
+        this.getData()
+        if (this.value) {
+            if (!this.multiple) {
+                this.getItem(this.value);
+            } else {
+                this.selected = _.map(this.value, item => {
+                    let el = JSON.parse(JSON.stringify(item));
+                    return {
+                        id: el.id,
+                        name: this.getItemTitle(el)
+                    }
+                });
+            }
         }
     },
     methods: {
@@ -103,13 +121,13 @@ export default {
         getItem(id = null) {
             if (this.resourceUrl && id) {
                 let index = this.resourceUrl.indexOf('?');
-                let url = (index>0)?this.resourceUrl.slice(0, index):this.resourceUrl;
+                let url = (index > 0) ? this.resourceUrl.slice(0, index) : this.resourceUrl;
                 axios.get(url + '/' + id).then(response => {
                     let item = response.data.data;
                     if (item.hasOwnProperty('id')) {
                         this.selected = {
                             id: item.id,
-                            name: item.attributes.name
+                            name: this.getTitle(item)
                         }
                     }
                 }).catch(error => {
@@ -117,24 +135,51 @@ export default {
                 });
             }
         },
+        getItemTitle(item) {
+            return item[this.labelAttribute];
+        },
+        getTitle(option) {
+            //For example
+            return option.attributes[this.labelAttribute];
+        },
         getData(searchQuery = '') {
             if (this.resourceUrl) {
                 let searchParams = '';
                 if (_.size(searchQuery) > 1) {
                     searchParams = '?';
-                    if(this.resourceUrl.indexOf('?')>0) searchParams = '&';
+                    if (this.resourceUrl.indexOf('?') > 0) searchParams = '&';
                     searchParams += 'filter[' + this.searchBy + ']=' + searchQuery;
                 }
 
                 axios.get(this.resourceUrl + searchParams).then(response => {
-                    this.data = _.map(response.data.data, item => {
-                        if (!_.isEmpty(item)) {
-                            return {
-                                id: item.id,
-                                name: item.attributes.name
+                    let options = [];
+                    if (searchQuery && this.useQuery) {
+                        let isFind = false;
+                        _.forEach(response.data.data, (option) => {
+                            if (searchQuery.toLowerCase() === this.getTitle(option).toLowerCase()) {
+                                isFind = true;
                             }
+                        });
+
+                        if (!isFind) {
+                            options.push({
+                                id: '0',
+                                name: searchQuery
+                            });
+                        }
+                    }
+
+                    _.forEach(response.data.data, item => {
+                        if (!_.isEmpty(item)) {
+                            options.push({
+                                id: item.id,
+                                name: this.getTitle(item)
+                            });
                         }
                     });
+
+                    this.data = options;
+
                 }).catch(error => {
                     console.log(error);
                 });
@@ -146,15 +191,16 @@ export default {
         selected(val) { //Для v-model в родитель
             let result = null;
             if (!_.isEmpty(val)) {
-                if (this.multiple) result = val;
-                else
+                if (this.multiple) {
+                    result = val;
+                } else
                     result = val.id;
             }
 
             this.$emit('input', result);
         },
         value(val) {
-            if(_.isEmpty(val)){
+            if (_.isEmpty(val)) {
                 this.selected = val;
             }
         }
