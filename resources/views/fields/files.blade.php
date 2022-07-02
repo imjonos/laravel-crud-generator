6389d1b0aad7f6be4b25ed90b274e93b3a94cfbd
Eugeny Nosenko <info@coders.studio>
2022-07-02 11:47:55 +0300

    Added comments to model

--- resources/views/fields/files.blade.php
+++ resources/views/fields/files.blade.php
@@ -1,10 +1,12 @@
 <div class="form-group">
-	<label for="">
-		{{ $label }}
-		@if($required ?? false)
-		<span class="text-danger">*</span>
-		@endif
-	</label>
-    <file-upload v-model="{{ $vModel }}" name="{{ $name }}" placeholder="{{ $placeholder }}"></file-upload>
-	<div class="text-danger" v-if="errors.has('{{ $name }}')" v-html="errors.first('{{ $name }}')"></div>
+    <label for="">
+        {{ $label }}
+        @if($required ?? false)
+            <span class="text-danger">*</span>
+        @endif
+    </label>
+    <file-upload v-model="{{ $vModel }}" @if(isset($options)) :options="{{ json_decode($options) }}"
+                 @endif name="{{ $name }}"
+                 placeholder="{{ $placeholder }}"></file-upload>
+    <div class="text-danger" v-if="errors.has('{{ $name }}')" v-html="errors.first('{{ $name }}')"></div>
 </div>
